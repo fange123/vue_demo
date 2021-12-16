c20d3e125f4e56da2db69c4be33af8b77c2d3f0c
张海玉 <2030564240@qq.com>
2021-12-16 18:04:15 +0800

    feat:todo计算属性学习

--- todo/todomvc-app-template-master/js/app.js
+++ todo/todomvc-app-template-master/js/app.js
@@ -9,21 +9,21 @@
           id: 1,
           name: "睡觉",
           flag: false,
-          isEdit: false,
         },
         {
           id: 2,
           name: "吃饭",
           flag: true,
-          isEdit: false,
         },
         {
           id: 3,
           name: "蹦迪",
           flag: true,
-          isEdit: false,
         },
       ],
+      active: "",
+      selectList: [],
+      type: "All",
     },
     methods: {
       deleteList(id) {
@@ -34,18 +34,55 @@
           id: this.list[this.list.length - 1].id + 1,
           name: this.task,
           flag: false,
-          isEdit: false,
         });
         this.task = "";
       },
       editList(id) {
-        this.list = this.list.map((item) =>
-          item.id === id ? (item.isEdit = true) : (item.isEdit = false)
-        );
+        this.active = id;
+      },
+      editTodo() {
+        this.active = "";
+      },
+      clearList() {
+        this.list = this.list.filter((item) => !item.flag);
       },
     },
+    //?指定计算属性
+    //?计算属性和data一样，也能提供给属性的，但是和data不一样，computed 里面的值必须经过计算得到的结果
+    //?计算属性在写法上是个函数，但是不能调用，计算逻辑写在函数中，返回值就是属性值
+    //~计算属性的性能非常高，基于缓存实现
+    //~计算属性一旦把计算结果结算出来，就会把结果存储起来，不管使用多少次，直接用
+    //~计算属性的值只会当它依赖的属性发生改变，计算属性的结果才会变
+    computed: {
+      activeNum: function () {
+        return this.list.filter((item) => !item.flag).length;
+      },
+      completedNum: function () {
+        return this.list.some((item) => item.flag);
+      },
+      isShow: function () {
+        return this.list.length > 0;
+      },
+      // filterTodo: function (type) {
+      //   this.type = type;
+      //   switch (type) {
+      //     case "All":
+      //       this.list = this.list;
+      //       break;
+      //     case "Active":
+      //       this.list = this.list.filter((item) => item.flag !== true);
+      //       break;
+      //     case "Completed":
+      //       this.list = this.list.filter((item) => item.flag === true);
+      //       break;
+      //     default:
+      //       break;
+      //   }
+      // },
+    },
+    mounted() {},
     updated() {
-      console.log(this.list);
+      console.log(this.selectList);
     },
   });
 })(window);
